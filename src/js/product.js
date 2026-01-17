5da40362cf182536e060ffbdbd9a14645bb9b859
DMHacker123 <matosalvarezdm@hotmail.com>
2026-01-17 16:48:28 -0400

    Complete individual activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,41 +1,27 @@
-<<<<<<< HEAD
-import { getParam } from "./utils.mjs";
-=======
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
->>>>>>> 58b8d33 ("Add item to cart broken")
+import { getParam, getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 const productID = getParam("product");
 
-<<<<<<< HEAD
 const product = new ProductDetails(productID, dataSource);
 product.init();
-=======
+
+// Add product to cart
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || [];
   cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
-  let cart = getLocalStorage("so-cart");
-
-  if (!Array.isArray(cart)) {
-    cart = [];
-  }
-
-  cart.push(product);
-  setLocalStorage("so-cart", cart);
 }
 
-// add to cart button event handler
+// Add to Cart button handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
+// Attach event listener
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
->>>>>>> 58b8d33 ("Add item to cart broken")
