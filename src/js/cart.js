fcb52f4984a214e49606cc906664decd9bdf7988
DMHacker123 <matosalvarezdm@hotmail.com>
2026-01-19 14:51:30 -0400

    Fix cart empty error

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,7 +2,6 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
-<<<<<<< HEAD
   const cartList = document.querySelector(".cart-list");
 
   if (!cartList) return;
@@ -16,19 +15,8 @@ function renderCartContents() {
   }
 
   cartList.innerHTML = cartItems
-    .map(item => cartItemTemplate(item))
+    .map((item) => cartItemTemplate(item))
     .join("");
-=======
-
-  if (cartItems.length === 0) {
-    document.querySelector(".product-list").innerHTML =
-      "<p>Your cart is empty.</p>";
-    return;
-  }
-
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
->>>>>>> 58b8d33 ("Add item to cart broken")
 }
 
 function cartItemTemplate(item) {
@@ -38,7 +26,6 @@ function cartItemTemplate(item) {
         <img src="${item.Image}" alt="${item.Name}" />
       </a>
 
-<<<<<<< HEAD
       <a href="#">
         <h2 class="card__name">${item.Name}</h2>
       </a>
@@ -52,20 +39,6 @@ function cartItemTemplate(item) {
       <p class="cart-card__price">
         $${item.FinalPrice}
       </p>
-=======
-  return newItem;
-  return `
-    <li class="cart-card divider">
-      <a href="#" class="cart-card__image">
-        <img src="${item.Image}" alt="${item.Name}" />
-      </a>
-      <a href="#">
-        <h2 class="card__name">${item.Name}</h2>
-      </a>
-      <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-      <p class="cart-card__quantity">qty: 1</p>
-      <p class="cart-card__price">$${item.FinalPrice}</p>
->>>>>>> 58b8d33 ("Add item to cart broken")
     </li>
   `;
 }
